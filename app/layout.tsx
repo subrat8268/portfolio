e423d7273b8006ec6b3e376d02041956f978b0a7
subrat8268 <subrato8268@gmail.com>
2026-05-17 11:49:24 +0530

    feat: replace old favicons with new PNG version for better compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,12 @@ export default function RootLayout({
         />
         <link
           rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon.png"
+        />
+        <link
+          rel="icon"
           type="image/x-icon"
           href="/favicon.ico"
         />
